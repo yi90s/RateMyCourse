341dc6363d5c64328956632e6f9a3832efe8b8f5
M <arjunwam@myumanitoba.ca>
2020-02-09 17:13:01 -0600

    Added classes for Student, Course and Faculty to the Controllers folder

---
+++ Controllers/Objects/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cReg_WebApp.Controllers
+{
+    public class Course
+    {
+        String name;
+        int id;
+        String desc;
+
+        public Course (String name, int id, String desc)
+        {
+            this.name = name;
+            this.id = id;
+            this.desc = desc;
+        }
+    }
+}
---
+++ Controllers/Objects/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cReg_WebApp.Controllers
+{
+    public class Student
+    {
+        String name;
+        int id;
+        Faculty major { get; set; }
+        Faculty minor { get; set; }
+
+        public Student (String name, int id)
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public Student (String name, int id, Faculty major, Faculty minor)
+        {
+            this.name = name;
+            this.id = id;
+            this.major = major;
+            this.minor = minor;
+        }
+    }
+}
