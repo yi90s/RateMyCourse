5e99a7e17eb1a136a7fd1c96503aac6f65d4ca31
Emile Bui <frostwyrm9x@gmail.com>
2020-03-19 01:28:18 -0500

    Refractor

--- src/cRegis.Mobile/Views/MasterPage.xaml.cs
+++ src/cRegis.Mobile/Views/MasterPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cReg_Mobile.ViewModels;
 using cReg_Mobile.Objects;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
