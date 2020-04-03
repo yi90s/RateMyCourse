8ecc112f24c417b1ddfdce38972d7e8d729a871a
M <arjunwam@myumanitoba.ca>
2020-04-03 18:47:44 -0500

    - Added test for:         Finding Course -> Registering for said Course -> Dropping the Course

--- cRegis.AcceptanceTests/AutomatedTests.cs
+++ cRegis.AcceptanceTests/AutomatedTests.cs
@@ -21,16 +21,68 @@ namespace cRegis.AcceptanceTests
             _driver.Dispose();
         }
 
-        //Tests
+        //Helpers
 
-        [Fact]
-        public void TestLogin()
+        private void signin()
         {
             _driver.Navigate().GoToUrl(testingUrl);
             _driver.FindElement(By.Id("UserName")).SendKeys("jb");
             _driver.FindElement(By.Id("Password")).SendKeys("Password1!");
             _driver.FindElement(By.Id("Password")).Submit();
+        }
+
+        private void signout()
+        {
+            _driver.FindElement(By.XPath("/html/body/header/nav/div/div[2]/input")).Click();
+        }
+
+        //TESTS
+
+        [Fact]
+        public void Signin_Signout_Test()
+        {
+            signin();
             Assert.Contains("Welcome, John Braico", _driver.PageSource);
+            signout();
+            Assert.DoesNotContain("Welcome, John Braico", _driver.PageSource);
+        }
+
+        [Fact]
+        public void FindCourse_RegisterCourse_Verify_DropCourse_Verify_Test()
+        {
+            signin();
+
+            //Verify the the student is currently not enrolled the "Calculus 2"
+            Assert.DoesNotContain("Calculus 2", _driver.PageSource);
+
+            //Click the "Find Course" Button
+            _driver.FindElement(By.LinkText("Find Course")).Click();
+            Assert.Contains("Calculus 2", _driver.PageSource);
+
+            //Click the "Register" button for Calculus 2
+            _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div[2]/div[2]/div/div[2]/div/a[2]")).Click();
+            //Click "yes" for the popup dialog box
+            _driver.SwitchTo().Alert().Accept();
+            Assert.DoesNotContain("Calculus 2", _driver.PageSource);
+
+            //Click the "jb" buttom
+            _driver.FindElement(By.LinkText("jb")).Click();
+            Assert.Contains("Calculus 2", _driver.PageSource);
+
+            //Click the "Drop" button for Calculus 2
+            _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div[7]/div[2]/div/div[2]/div/button")).Click();
+            //Click "yes" for the popup dialog boxes
+            _driver.SwitchTo().Alert().Accept();
+            _driver.SwitchTo().Alert().Accept();
+
+            //Ensure that "Calculus 2" is no displayed as Registered
+            Assert.DoesNotContain("Calculus 2", _driver.PageSource);
+
+            //Click the "Find Course" Button
+            _driver.FindElement(By.LinkText("Find Course")).Click();
+            Assert.Contains("Calculus 2", _driver.PageSource);
+
+            signout();
         }
     }
 }
