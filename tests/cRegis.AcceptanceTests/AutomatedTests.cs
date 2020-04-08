1c9e320e7219d2e72891e50a33f79d3f545235ca
M <arjunwam@myumanitoba.ca>
2020-04-08 16:30:20 -0500

    - Updated Acceptance tests to work with updated aws instance. - Added new tests to match updated features

--- tests/cRegis.AcceptanceTests/AutomatedTests.cs
+++ tests/cRegis.AcceptanceTests/AutomatedTests.cs
@@ -1,4 +1,4 @@
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using System;
 using Xunit;
@@ -9,7 +9,7 @@ namespace cRegis.AcceptanceTests
     {
         private Random rand = new Random();
         private readonly IWebDriver _driver;
-        private string testingUrl = "http://ec2-15-222-137-75.ca-central-1.compute.amazonaws.com/";
+        private string testingUrl = "http://creg.ca-central-1.elasticbeanstalk.com/";
 
         public AutomatedTests()
         {
@@ -18,6 +18,7 @@ namespace cRegis.AcceptanceTests
 
             signin();
             clearRegistrations();
+            clearWishlist();
         }
 
         public void Dispose()
@@ -55,7 +56,7 @@ namespace cRegis.AcceptanceTests
             //Click "yes" for the popup dialog box
             _driver.SwitchTo().Alert().Accept();
 
-            //Click the "jb" button
+            //Click the home button
             _driver.FindElement(By.LinkText("jb")).Click();
 
             return courseName;
@@ -65,9 +66,13 @@ namespace cRegis.AcceptanceTests
         {
             //Click the "Drop" button for the Course
             _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div/div[2]/div/div[2]/div/button")).Click();
+
             //Click "yes" for the popup dialog boxes
             _driver.SwitchTo().Alert().Accept();
             _driver.SwitchTo().Alert().Accept();
+
+            //Click the home button
+            _driver.FindElement(By.LinkText("jb")).Click();
         }
 
         private void clearRegistrations()
@@ -76,6 +81,44 @@ namespace cRegis.AcceptanceTests
             {
                 dropCourse();
             }
+
+            //Click the home button
+            _driver.FindElement(By.LinkText("jb")).Click();
+        }
+
+        private string addCourseToWishlist(int level = 1)
+        {
+            //Click the "Find Course" Button
+            _driver.FindElement(By.LinkText("Find Course")).Click();
+
+            //Save the name of the top Course
+            string courseName = _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div[" + level + "]/div[1]")).Text;
+
+            //Click the "Add To Wishlist" button for the Course
+            _driver.FindElement(By.XPath("/html/body/div/main/div[2]/div[" + level + "]/div[2]/div/div[2]/div/a[3]")).Click();
+            //Click "yes" for the popup dialog box
+            _driver.SwitchTo().Alert().Accept();
+
+            //Click the home button
+            _driver.FindElement(By.LinkText("jb")).Click();
+
+            return courseName;
+        }
+
+        private void clearWishlist()
+        {
+            _driver.FindElement(By.LinkText("Wishlist")).Click();
+
+            while (_driver.FindElements(By.LinkText("🗙")).Count != 0)
+            {
+                //Remove the topmost Course
+                _driver.FindElement(By.LinkText("🗙")).Click();
+                //Click "yes" for the popup dialog box
+                _driver.SwitchTo().Alert().Accept();
+            }
+
+            //Click the home button
+            _driver.FindElement(By.LinkText("jb")).Click();
         }
 
         //Tests
@@ -83,7 +126,7 @@ namespace cRegis.AcceptanceTests
         [Fact]
         public void StudentInfo_Test()
         {
-            //Verify that the student info is displayed (ie. signin worked)
+            //Verify that the student info is displayed
             Assert.Contains("Welcome, John Braico", _driver.PageSource);
             Assert.Contains("Student Name : John Braico", _driver.PageSource);
             Assert.Contains("Student Id : 1", _driver.PageSource);
@@ -145,6 +188,33 @@ namespace cRegis.AcceptanceTests
         }
 
         [Fact]
+        public void FindCourse_AddToWishlist_RemoveFromWishlist_Test()
+        {
+            string courseName = addCourseToWishlist(1);
+
+            //Click the "Wishlist" Button
+            _driver.FindElement(By.LinkText("Wishlist")).Click();
+
+            //Verify that the course is in the Waitlist
+            Assert.Contains(courseName, _driver.PageSource);
+
+            //Remove the Course from the wishlist
+            //_driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/div[2]/div/div[2]/div/a[5]")).Click();
+            _driver.FindElement(By.LinkText("🗙")).Click();
+            //Click "yes" for the popup dialog box
+            _driver.SwitchTo().Alert().Accept();
+
+            //Verify that the course is in the Waitlist
+            Assert.DoesNotContain(courseName, _driver.PageSource);
+
+            //Click the "Find Course" Button
+            _driver.FindElement(By.LinkText("Find Course")).Click();
+
+            //Verify that the course is in the Waitlist
+            Assert.Contains(courseName, _driver.PageSource);
+        }
+
+        [Fact]
         public void History_ViewDetails_Test()
         {
             //Click the "History" Button
@@ -198,6 +268,76 @@ namespace cRegis.AcceptanceTests
             //Verify that the rating was added
             Assert.Contains(rating, _driver.PageSource);
             Assert.Contains(randomComment, _driver.PageSource);
+        }
+
+        [Fact]
+        public void Wishlist_Detail_Test()
+        {
+            string courseName = addCourseToWishlist(1);
+
+            //Click the "Wishlist" button
+            _driver.FindElement(By.LinkText("Wishlist")).Click();
+
+            //Click the "Detail" button for the Course
+            _driver.FindElement(By.LinkText("Detail")).Click();
+
+            //Verify that it shows the Details for that Course
+            Assert.Contains("Available Space", _driver.PageSource);
+            Assert.Contains("Date", _driver.PageSource);
+            Assert.Contains("Average Rating", _driver.PageSource);
+            Assert.Contains("Instructor", _driver.PageSource);
+            Assert.Contains("Location", _driver.PageSource);
+        }
+
+        [Fact]
+        public void Wishlist_Register_Test()
+        {
+            string courseName = addCourseToWishlist(1);
+
+            //Click the "Wishlist" button
+            _driver.FindElement(By.LinkText("Wishlist")).Click();
+
+            //Click the "Register" button for the Course
+            _driver.FindElement(By.LinkText("Register")).Click();
+            //Click "yes" for the popup dialog box
+            _driver.SwitchTo().Alert().Accept();
+
+            //Click the home button
+            _driver.FindElement(By.LinkText("jb")).Click();
+
+            //Verify that the course is registered for
+            Assert.Contains(courseName, _driver.PageSource);
+        }
+
+        [Fact]
+        public void Wishlist_MoveUp_MoveDown_Test()
+        {
+            addCourseToWishlist(1);
+            addCourseToWishlist(2);
+
+            //Click the "Wishlist" button
+            _driver.FindElement(By.LinkText("Wishlist")).Click();
+
+            string topName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/div[1]")).Text;
+            string bottomName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div[1]")).Text;
+
+            //Click the "move down" button for the top Course
+            _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/div[2]/div/div[2]/div/a[4]")).Click();
+
+            string currTopName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/div[1]")).Text;
+            string currBottomName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div[1]")).Text;
+
+            Assert.Equal(bottomName, currTopName);
+            Assert.Equal(topName, currBottomName);
+
+            //Click the "move up" button for the botton Course
+            _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div[2]/div/div[2]/div/a[3]")).Click();
+
+            currTopName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[1]/div[1]")).Text;
+            currBottomName = _driver.FindElement(By.XPath("/html/body/div/main/div/div[2]/div[1]")).Text;
+
+            Assert.Equal(topName, currTopName);
+            Assert.Equal(bottomName, currBottomName);
         }
     }
 }
